36261c46cdf5761d92cc171a2c6b5f5c09ea3eec
dbwlgns777 <52645533+dbwlgns777@users.noreply.github.com>
2026-04-13 21:36:43 +0900

    Print sensor values 1..99 in a single log line

--- src/main/java/com/zes/device/config/ZES_MongoConfig.java
+++ src/main/java/com/zes/device/config/ZES_MongoConfig.java
@@ -11,6 +11,7 @@ public class ZES_MongoConfig
     private static final String ZES_gv_uri = "mongodb://localhost:27017";
     public static final String ZES_gv_databaseName = "TIS_POF_SCADA";
     public static final String ZES_gv_vibrationCollection = "POF_Vibration";
+    public static final String ZES_gv_strainGaugeCollection = "POF_StrainGauge";
     public static final String ZES_gv_temperatureCollection = "POF_Temperature";
 
     private static final MongoClient ZES_gv_mongoClient = MongoClients.create(ZES_gv_uri);
